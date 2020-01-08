59b92f7cd5e301f7273b8375d69e8e71153e6800
Xing Tao Shi <xshi@stuy.edu>
2020-01-08 15:01:32 -0500

    did the forms part of the assignment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,61 +3,75 @@ import PropTypes from 'prop-types';
 
 import './App.css';
 
-
 class FormEdit extends Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    firstName: this.props.firstName,
+	    lastName: this.props.lastName,
+	    currState: this.props.currState
+	};
 
-  constructor(props) {
-    super()
+	this.onMouseDown1 = this.onMouseDown1.bind(this);
+	this.onMouseDown2 = this.onMouseDown2.bind(this);
+	this.onMouseDown3 = this.onMouseDown3.bind(this);
+    }
+    
+    onMouseDown1() {
+	this.setState({currState: "editing"});
+    }
 
-    this.state = {
-      clicks: this.props.clicks
-    };
+    onMouseDown2() {
+	this.setState({currState: "default", firstName: document.getElementById("newFirst").value, lastName:document.getElementById("newLast").value});
+    }
 
-  }
+    onMouseDown3() {
+	this.setState({currState: "default"});
+    }
 
-  render() {
-
-    return (
-            
-          <div className="container">
-            <div>
-              {this.props.firstName}
-          
-            </div>
-
-            <br />
-
-            <div>
-              {this.props.lastName}
-            </div>
-
-            <br/>
-
-            <button>Edit</button>
-            
-          </div>
-            
- 
-    );
-
-  }
-
-}
-
-function App() {
-  return (
-    <div className="App">
-      <br/>
-      <FormEdit firstName="First Name: " lastName="Last Name:"/>
-
-    </div>
-  );
+    render() {
+	if (this.state.currState == "default") {
+	    return (
+		    <div> 		    
+		    First Name: {this.state.firstName} <br/>
+		    Last Name: {this.state.lastName} <br/>
+		    <button onMouseDown={this.onMouseDown1}>Edit</button>
+		    </div>
+		    );
+	}
+	else {
+	    return (
+		    <div>
+		    <input type="text" id="newFirst"/>
+		    <input type="text" id="newLast"/>
+		    <button onMouseDown={this.onMouseDown2}>Submit</button>
+		    <button onMouseDown={this.onMouseDown3}>Cancel</button>		    
+		    </div>
+		    );
+	}
+    }   
 }
 
 
 FormEdit.propTypes = {
-  firstName: PropTypes.string.isRequired,
-  lastName: PropTypes.string.isRequired
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    currState: PropTypes.string.isRequired
+};
+
+FormEdit.defaultProps = {
+    firstName: "John",
+    lastName: "Doe",
+    currState: "default"
+};
+
+function App() {
+    return (
+	    <div className="App">
+	    <FormEdit firstName="Xing Tao" lastName="Shi" currState="default"/>
+	    </div>
+	    );
 }
+
 
 export default App;
